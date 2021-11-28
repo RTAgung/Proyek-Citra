1fcc2dd84ee218863a9eef9c43493611da81e716
Allyandaru <all4yandaru@gmail.com>
2021-11-28 16:47:14 +0700

    import image success

--- GUI2.py
+++ GUI2.py
@@ -1,20 +1,26 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, NW, filedialog
 from tkinter import filedialog as fd
 from tkinter.messagebox import showinfo
+
+import cv2
+from PIL import Image, ImageTk
 
 window = tk.Tk()
 window.geometry("1280x720")
 
 filename = ''
+matrix_img = None
 
 
-def select_file():
+def open_file():
     global filename
 
     filetypes = (
         ('image files', '*.jpeg'),
+        ('image files', '*.JPEG'),
         ('image files', '*.jpg'),
+        ('image files', '*.JPG'),
         ('image files', '*.png'),
     )
 
@@ -28,10 +34,28 @@ def select_file():
         message=filename
     )
 
+    open_img()
+
+
+def open_img():
+    global matrix_img
+    global panel
+    x = filename
+
+    matrix_img = cv2.imread(x)
+    matrix_img = cv2.resize(matrix_img, dsize=(480, 270), interpolation=cv2.INTER_CUBIC)
+    matrix_img = cv2.cvtColor(matrix_img, cv2.COLOR_BGR2RGB)
+
+    img = ImageTk.PhotoImage(image=Image.fromarray(matrix_img))
+
+    panel = tk.Label(citra_left, image=img)
+    panel.image = img
+    panel.pack()
+
 
 # create a toplevel menu
 menubar = tk.Menu()
-menubar.add_command(label="Open File", command=select_file)
+menubar.add_command(label="Open File", command=open_file)
 menubar.add_command(label="Save File", command=window.quit)
 
 # display the menu
@@ -46,7 +70,7 @@ top_left = tk.Frame(master=frame_top, bd=1, width=640, height=420, relief="raise
 top_left.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
 
 # citra left
-citra_left = tk.Frame(master=top_left, bd=1, width=640, height=320, relief="raised")
+citra_left = tk.Frame(master=top_left, bd=1, width=640, height=280, relief="raised")
 citra_left.pack(fill=tk.BOTH, side=tk.TOP, expand=True)
 
 # hist left
@@ -58,7 +82,7 @@ top_right = tk.Frame(master=frame_top, bd=1, width=640, height=420, relief="rais
 top_right.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
 
 # citra right
-citra_right = tk.Frame(master=top_right, bd=1, width=640, height=320, relief="raised")
+citra_right = tk.Frame(master=top_right, bd=1, width=640, height=280, relief="raised")
 citra_right.pack(fill=tk.BOTH, side=tk.TOP, expand=True)
 
 # hist right
@@ -84,3 +108,22 @@ bot_right = tk.Frame(master=frame_bottom, bd=1, width=640, height=250, relief="r
 bot_right.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
 
 window.mainloop()
+
+
+'''
+def open_img():
+global matrix_img
+x = filename
+matrix_img = cv2.imread(x)
+matrix_img = cv2.resize(matrix_img, dsize=(480, 270), interpolation=cv2.INTER_CUBIC)
+img = ImageTk.PhotoImage(image=Image.fromarray(matrix_img))
+panel = tk.Label(citra_left, image=img)
+panel.image = img
+panel.pack()
+
+
+canvas = tk.Canvas(citra_left, width=300, height=300)
+canvas.pack()
+img = ImageTk.PhotoImage(Image.open(filename))
+canvas.create_image(20, 20, anchor=NW, image=img)
+'''
