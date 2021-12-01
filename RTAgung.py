fce131d07f62fb1dfa8c7792a7dc3dd822098b9d
Rama Tri Agung <ramatriagung91@gmail.com>
2021-12-01 11:21:27 +0700

    add edge detection in class RTAgung.py

--- RTAgung.py
+++ RTAgung.py
@@ -40,17 +40,35 @@ class RTAgung:
         image = image.astype('int16')
         image += value
         image = np.where(image > 255, 255,
-                              np.where(image < 0, 0, image))
+                         np.where(image < 0, 0, image))
         image = image.astype('uint8')
         return image
 
+    def edge_detection(self, image=None):
+        if image is None:
+            image = self.image
+        kernel = np.array([[-1, -1, -1],
+                           [-1, 8, -1],
+                           [-1, -1, -1]])
+        image = self.to_grayscale(image)
+        image = cv2.filter2D(src=image, ddepth=-1, kernel=kernel)
+        return image
 
-# file = "D:\\Photo\\1 Syawwal 1440H\\_MG_9252.JPG"
-file = "C:\\Users\\User\\OneDrive\\Pictures\\Front_wall.png"
 
+file = "ndr.JPG"
 img = cv2.imread(file)
-# img = np.ones((400, 400)) * 125
-# img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 img = cv2.resize(img, (400, 400), interpolation=cv2.INTER_CUBIC)
 
 rta = RTAgung(img)
+
+# to_grayscale
+rta.show_img(rta.to_grayscale())
+
+# to_negative
+rta.show_img(rta.to_negative())
+
+# brightening
+rta.show_img(rta.brightening(100))
+
+# edge_detection
+rta.show_img(rta.edge_detection())
