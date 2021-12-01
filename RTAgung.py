b8060114670e7701fa5c926728feabaaa12e472e
Rama Tri Agung <ramatriagung91@gmail.com>
2021-12-01 09:17:08 +0700

    update method in class RTAgung.py

--- RTAgung.py
+++ RTAgung.py
@@ -9,29 +9,40 @@ class RTAgung:
     def update_img(self, image):
         self.image = image
 
-    def show_img(self):
-        cv2.imshow('image effect', self.image)
+    def show_img(self, image=None):
+        if image is None:
+            image = self.image
+        cv2.imshow('image effect', image)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-    def describe(self):
-        print(self.image.shape)
-        print(self.image[:2][:2])
+    def describe(self, image=None):
+        if image is None:
+            image = self.image
+        print(image.shape)
+        print(image[:2][:2])
 
-    def to_grayscale(self):
-        self.image = cv2.cvtColor(self.image, cv2.COLOR_RGB2GRAY)
+    def to_grayscale(self, image=None):
+        if image is None:
+            image = self.image
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+        return image
 
-    def to_negative(self):
-        self.image = cv2.bitwise_not(self.image)
+    def to_negative(self, image=None):
+        if image is None:
+            image = self.image
+        image = cv2.bitwise_not(image)
+        return image
 
-    def brightening(self, value):
-        self.image = self.image.astype('int16')
-        self.image += value
-        self.image = np.where(self.image > 255, 255,
-                              np.where(self.image < 0, 0, self.image))
-        self.image = self.image.astype('uint8')
-
-
+    def brightening(self, value, image=None):
+        if image is None:
+            image = self.image
+        image = image.astype('int16')
+        image += value
+        image = np.where(image > 255, 255,
+                              np.where(image < 0, 0, image))
+        image = image.astype('uint8')
+        return image
 
 
 file = "D:\\Photo\\1 Syawwal 1440H\\_MG_9252.JPG"
@@ -42,13 +53,3 @@ img = cv2.imread(file)
 img = cv2.resize(img, (400, 400), interpolation=cv2.INTER_CUBIC)
 
 rta = RTAgung(img)
-
-rta.describe()
-rta.show_img()
-
-rta.brightening(100)
-rta.describe()
-rta.show_img()
-
-
-
